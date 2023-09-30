f405564d52daa69a76bac58a4c0464b66a44e31a
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-09-30 19:28:34 +0400

    successfully masked imageview by shape of png image in it

--- ViewMasking/ViewController.swift
+++ ViewMasking/ViewController.swift
@@ -1,19 +1,55 @@
-//
-//  ViewController.swift
-//  ViewMasking
-//
-//  Created by Suren Poghosyan on 30.09.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let imageSize = CGSize(width: 200, height: 200) // Set your preferred size
+
+        
+        // Set the image for the image view
+        if let image = UIImage(named: "firebase") {
+            imageView.image = resizeImage(image, targetSize: imageSize)
+            
+            
+            
+            let maskLayer = CAShapeLayer()
+            maskLayer.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            maskLayer.contents = image.cgImage
+            
+            
+            
+            imageView.backgroundColor = .red
+            
+            
+            imageView.layer.mask = maskLayer
+            
+            
+            view.addSubview(imageView)
+            
+            imageView.image = image
+        }
+        // Set constraints for the image view (optional)
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200), // Set your preferred width
+            imageView.heightAnchor.constraint(equalToConstant: 200), // Set your preferred height
+        ])
     }
-
-
 }
 
+func resizeImage(_ image: UIImage, targetSize: CGSize) -> UIImage {
+       let rect = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height)
+       UIGraphicsBeginImageContextWithOptions(targetSize, false, 0.0)
+       image.draw(in: rect)
+       let newImage = UIGraphicsGetImageFromCurrentImageContext()
+       UIGraphicsEndImageContext()
+       return newImage ?? image
+   }
